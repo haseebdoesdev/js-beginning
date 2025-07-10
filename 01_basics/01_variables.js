fae29a96244af6fa5dc4be73cca55dfbb6e0860f
Abdul Haseeb <abdlhaseeb17@gmail.com>
2025-07-10 09:30:14 +0000

    well, soem basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const MTU = 1500
+let transmissionRate = 2500
+
+bandwidth = 100000
+
+console.table([MTU, transmissionRate,bandwidth])
---
+++ 01_basics/02_datatypesAndConversion.js
@@ -0,0 +1,21 @@
+let someNumberInString = "33a"
+console.log(typeof someNumberInString)
+
+let someNumber = 33 
+console.log(typeof someNumber)
+
+let isLoggedIn = false
+console.log(typeof isLoggedIn)
+
+let someNull = null
+let someUndefinedVar
+
+
+console.log(someUndefinedVar)
+
+console.log("1" + 2+ 3 + 2)
+console.log(23+1+"2")
+let someIncrementalVal = 20 
+console.log(someIncrementalVal++)
+console.log(++someIncrementalVal)
+
